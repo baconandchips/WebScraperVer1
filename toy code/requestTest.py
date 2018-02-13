663d429814d9529b0c4a5a691403d1dddee39e0b
Guanting Li <guanting009@yahoo.com>
2018-02-12 19:16:58 -0800

    second commit! added another small file

---
+++ toy code/requestTest.py
@@ -0,0 +1,9 @@
+import requests
+
+params = {"q": "pizza"}
+r = requests.get("http://bing.com", params = params)
+print("statusL", r.status_code)
+
+print(r.url)
+f = open("./page.html", "w+")
+f.write(r.text)
